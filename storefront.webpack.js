f4675fc679118fa68f1fa124bf99c29b87e44867
matheusgnreis <matheus@e-com.club>
2023-06-01 18:08:51 -0300

    chore: limit only for logged users

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,6 +5,7 @@ module.exports = () => ({
     alias: {
       './html/CartItem.html': path.resolve(__dirname, 'template/js/custom-js/html/CartItem.html'),
       './js/CartItem.js': path.resolve(__dirname, 'template/js/custom-js/js/CartItem.js'),
+      './js/LoginBlock.js': path.resolve(__dirname, 'template/js/custom-js/js/LoginBlock.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
       './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html')
