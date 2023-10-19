cd1ebaa4f0ee3657b2ba0ba1bf19280439c96294
matheusgnreis <matheus@e-com.club>
2023-10-19 15:52:11 -0300

    chore: create cart

--- template/js/index.js
+++ template/js/index.js
@@ -1,2 +1,9 @@
 import '#template/js/'
 import './custom-js/pages'
+import ecomCart from '@ecomplus/shopping-cart'
+import { fetchCart, fetchingCartId, upsertCart } from '@ecomplus/storefront-app/src/lib/sync-cart-to-api'
+upsertCart().then(() => {
+    ecomCart.on('change', () => {
+        upsertCart()
+    })
+})
