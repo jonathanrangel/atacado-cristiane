8abeceb5fe46ff82cbf4706114ccc9b5f22c6367
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-10-24 15:12:00 -0300

    chore: force cart after insert item

--- template/js/index.js
+++ template/js/index.js
@@ -2,8 +2,28 @@ import '#template/js/'
 import './custom-js/pages'
 import ecomCart from '@ecomplus/shopping-cart'
 import { fetchCart, fetchingCartId, upsertCart } from '@ecomplus/storefront-app/src/lib/sync-cart-to-api'
-upsertCart().then(() => {
+ecomCart.on('change', () => {
+    window.location = '/app/#/cart/'
+})
+/* upsertCart().then(() => {
     ecomCart.on('change', () => {
         upsertCart()
     })
 })
+
+if (ecomCart.data.) {
+    fetchCart(params.id)
+  }
+  fetchingCartId.then(id => {
+    if (params.id !== id && router.currentRoute.name === name) {
+      router.push({
+        name,
+        params: { ...params, id }
+      })
+      const { hostname, href } = window.location
+      if (/\.[a-z]+$/.test(hostname)) {
+        ecomCart.data.permalink = href
+        setTimeout(upsertCart, 500)
+      }
+    }
+  }) */
